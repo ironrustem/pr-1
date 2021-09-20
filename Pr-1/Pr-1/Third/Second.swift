c3f7bbb66ba76bf63f7d000416c20b81b79adec0
Rustem <47086556+ironrustem@users.noreply.github.com>
2021-09-20 19:31:26 +0300

    [MADE] made third task.

---
+++ Pr-1/Pr-1/Third/Second.swift
@@ -0,0 +1,18 @@
+//
+//  Second.swift
+//  Pr-1
+//
+//  Created by wrustem on 20.09.2021.
+//
+
+import Foundation
+
+class MainClass1: NSObject {
+    dynamic func method1() {
+        print("method1")
+    }
+}
+
+// let a = MainClass()
+// a.method1()
+// Message dispath
